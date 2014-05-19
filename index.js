d0dfc50b76120deb9bd5db02aa746050007ee7de
Michael Kaminsky <mkaminsky11@gmail.com>
2014-05-19 13:24:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 $("#js_div").hover(function(){
     $("#js_p").slideToggle( "slow", "linear" );
   },function(){
-    $("#js_p").slideToggle( "slow", "linear" );
 });
-
+$("#js_p").slideToggle( "slow", "linear" );
+//
 $("#css_div").hover(function(){
     $("#css_p").slideToggle( "slow", "linear" );
   },function(){
-    $("#css_p").slideToggle( "slow", "linear" );
 });
-
+$("#css_p").slideToggle( "slow", "linear" );
+//
 $("#html_div").hover(function(){
     $("#html_p").slideToggle( "slow", "linear" );
   },function(){
-    $("#html_p").slideToggle( "slow", "linear" );
 });
-
+$("#html_p").slideToggle( "slow", "linear" );
+//
 $("#sql_div").hover(function(){
     $("#sql_p").slideToggle( "slow", "linear" );
   },function(){
-    $("#sql_p").slideToggle( "slow", "linear" );
 });
-
+$("#sql_p").slideToggle( "slow", "linear" );
+//
 $("#java_div").hover(function(){
     $("#java_p").slideToggle( "slow", "linear" );
   },function(){
-    $("#java_p").slideToggle( "slow", "linear" );
 });
-
+$("#java_p").slideToggle( "slow", "linear" );
+//
 $("#c_div").hover(function(){
     $("#c_p").slideToggle( "slow", "linear" );
   },function(){
-    $("#c_p").slideToggle( "slow", "linear" );
 });
+ $("#c_p").slideToggle( "slow", "linear" );
