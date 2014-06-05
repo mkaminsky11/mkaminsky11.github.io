5659666753dfd66895d9a74c6ceaa769ec29e006
Michael Kaminsky <mkaminsky11@gmail.com>
2014-06-05 19:16:22 -0400

    Update intro.swift

--- lib/intro.swift
+++ lib/intro.swift
@@ -1,166 +1,38 @@
-//
-//  GameScene.swift
-//  FlappyBird
-//
-//  Created by Nate Murray on 6/2/14.
-//  Copyright (c) 2014 Fullstack.io. All rights reserved.
-//
+/*
+ * RootViewController.swift
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *                    Version 2, December 2004
+ *
+ * Copyright (C) 2013-2014 Su Yeol Jeon
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  0. You just DO WHAT THE FUCK YOU WANT TO.
+ *
+ */
 
-import SpriteKit
+import UIKit
 
-class GameScene: SKScene {
-    var bird = SKSpriteNode()
-    var skyColor = SKColor()
-    var verticalPipeGap = 150.0
-    var pipeTextureUp = SKTexture()
-    var pipeTextureDown = SKTexture()
-    var movePipesAndRemove = SKAction()
-    
-    override func didMoveToView(view: SKView) {
-        // setup physics
-        self.physicsWorld.gravity = CGVectorMake( 0.0, -5.0 )
-        
-        // setup background color
-        skyColor = SKColor(red: 81.0/255.0, green: 192.0/255.0, blue: 201.0/255.0, alpha: 1.0)
-        self.backgroundColor = skyColor
-        
-        // ground
-        var groundTexture = SKTexture(imageNamed: "land")
-        groundTexture.filteringMode = SKTextureFilteringMode.Nearest
-        
-        var moveGroundSprite = SKAction.moveByX(-groundTexture.size().width * 2.0, y: 0, duration: NSTimeInterval(0.02 * groundTexture.size().width * 2.0))
-        var resetGroundSprite = SKAction.moveByX(groundTexture.size().width * 2.0, y: 0, duration: 0.0)
-        var moveGroundSpritesForever = SKAction.repeatActionForever(SKAction.sequence([moveGroundSprite,resetGroundSprite]))
-        
-        for var i:CGFloat = 0; i < 2.0 + self.frame.size.width / ( groundTexture.size().width * 2.0 ); ++i {
-            var sprite = SKSpriteNode(texture: groundTexture)
-            sprite.setScale(2.0)
-            sprite.position = CGPointMake(i * sprite.size.width, sprite.size.height / 2.0)
-            sprite.runAction(moveGroundSpritesForever)
-            self.addChild(sprite)
-        }
-        
-        // skyline
-        var skyTexture = SKTexture(imageNamed: "sky")
-        skyTexture.filteringMode = SKTextureFilteringMode.Nearest
-        
-        var moveSkySprite = SKAction.moveByX(-skyTexture.size().width * 2.0, y: 0, duration: NSTimeInterval(0.1 * skyTexture.size().width * 2.0))
-        var resetSkySprite = SKAction.moveByX(skyTexture.size().width * 2.0, y: 0, duration: 0.0)
-        var moveSkySpritesForever = SKAction.repeatActionForever(SKAction.sequence([moveSkySprite,resetSkySprite]))
-        
-        for var i:CGFloat = 0; i < 2.0 + self.frame.size.width / ( skyTexture.size().width * 2.0 ); ++i {
-            var sprite = SKSpriteNode(texture: skyTexture)
-            sprite.setScale(2.0)
-            sprite.zPosition = -20;
-            sprite.position = CGPointMake(i * sprite.size.width, sprite.size.height / 2.0 + groundTexture.size().height * 2.0)
-            sprite.runAction(moveSkySpritesForever)
-            self.addChild(sprite)
-        }
-        
-        // create the pipes textures
-        pipeTextureUp = SKTexture(imageNamed: "PipeUp")
-        pipeTextureUp.filteringMode = SKTextureFilteringMode.Nearest
-        pipeTextureDown = SKTexture(imageNamed: "PipeDown")
-        pipeTextureDown.filteringMode = SKTextureFilteringMode.Nearest
-        
-        // create the pipes movement actions
-        var distanceToMove = CGFloat(self.frame.size.width + 2.0 * pipeTextureUp.size().width);
-        var movePipes = SKAction.moveByX(-distanceToMove, y:0.0, duration:NSTimeInterval(0.01 * distanceToMove));
-        var removePipes = SKAction.removeFromParent();
-        movePipesAndRemove = SKAction.sequence([movePipes, removePipes]);
-        
-        // spawn the pipes
-        var spawn = SKAction.runBlock({() in self.spawnPipes()})
-        var delay = SKAction.waitForDuration(NSTimeInterval(2.0))
-        var spawnThenDelay = SKAction.sequence([spawn, delay])
-        var spawnThenDelayForever = SKAction.repeatActionForever(spawnThenDelay)
-        self.runAction(spawnThenDelayForever)
-        
-        // setup our bird
-        var birdTexture1 = SKTexture(imageNamed: "bird-01")
-        birdTexture1.filteringMode = SKTextureFilteringMode.Nearest
-        var birdTexture2 = SKTexture(imageNamed: "bird-02")
-        birdTexture2.filteringMode = SKTextureFilteringMode.Nearest
-        
-        var anim = SKAction.animateWithTextures([birdTexture1, birdTexture2], timePerFrame: 0.2)
-        var flap = SKAction.repeatActionForever(anim)
-        
-        bird = SKSpriteNode(texture: birdTexture1)
-        bird.setScale(2.0)
-        bird.position = CGPoint(x: self.frame.size.width * 0.35, y:self.frame.size.height * 0.6)
-        bird.runAction(flap)
-        
-        
-        bird.physicsBody = SKPhysicsBody(circleOfRadius: bird.size.height / 2.0)
-        bird.physicsBody.dynamic = true
-        bird.physicsBody.allowsRotation = false
-        
-        self.addChild(bird)
-        
-        // create the ground
-        var ground = SKNode()
-        ground.position = CGPointMake(0, groundTexture.size().height)
-        ground.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(self.frame.size.width, groundTexture.size().height * 2.0))
-        ground.physicsBody.dynamic = false
-        self.addChild(ground)
-        
-        
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        var button: UIButton = UIButton.buttonWithType(UIButtonType.System) as UIButton
+        button.frame = CGRectMake(10, 10, 300, 60)
+        button.setTitle("Show", forState: UIControlState.Normal)
+        button.addTarget(self, action: "showButtonTouchUpInside", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(button)
     }
-    
-    func spawnPipes() {
-        var pipePair = SKNode()
-        pipePair.position = CGPointMake( self.frame.size.width + pipeTextureUp.size().width * 2, 0 );
-        pipePair.zPosition = -10;
-        
-        var height = UInt32( self.frame.size.height / 4 )
-        var y = arc4random() % height + height;
-        
-        var pipeDown = SKSpriteNode(texture: pipeTextureDown)
-        pipeDown.setScale(2.0)
-        pipeDown.position = CGPointMake(0.0, CGFloat(y) + pipeDown.size.height + CGFloat(verticalPipeGap))
-        
-        
-        pipeDown.physicsBody = SKPhysicsBody(rectangleOfSize: pipeDown.size)
-        pipeDown.physicsBody.dynamic = false
-        pipePair.addChild(pipeDown)
-        
-        var pipeUp = SKSpriteNode(texture: pipeTextureUp)
-        pipeUp.setScale(2.0)
-        pipeUp.position = CGPointMake(0.0, CGFloat(y))
-        
-        pipeUp.physicsBody = SKPhysicsBody(rectangleOfSize: pipeUp.size)
-        pipeUp.physicsBody.dynamic = false
-        pipePair.addChild(pipeUp)
-        
-        pipePair.runAction(movePipesAndRemove);
-        self.addChild(pipePair)
-    }
-    
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            bird.physicsBody.velocity = CGVectorMake(0, 0)
-            bird.physicsBody.applyImpulse(CGVectorMake(0, 30))
-            
-        }
-    }
-    
-    func clamp(min: CGFloat, max: CGFloat, value: CGFloat) -> CGFloat {
-        if( value > max ) {
-            return max;
-        } else if( value < min ) {
-            return min;
-        } else {
-            return value;
-        }
-    }
-    
-    
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-        bird.zRotation = self.clamp( -1, max: 0.5, value: bird.physicsBody.velocity.dy * ( bird.physicsBody.velocity.dy < 0 ? 0.003 : 0.001 ) );
+
+    func showButtonTouchUpInside() {
+        JLToast.makeText("Basic JLToast").show()
+        JLToast.makeText("You can set duration. `JLToastDelay.ShortDelay` means 2 seconds." +
+                         "`JLToastDelay.LongDelay` means 3.5 seconds.", duration: JLToastDelay.LongDelay).show()
+        JLToast.makeText("With delay, JLToast will be shown after delay.", delay: 1, duration: 5)
     }
 }
