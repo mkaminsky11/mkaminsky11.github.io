f45d23d8f1f2e0ae8c849349eb5848846d1e5ee4
Michael Kaminsky <mkaminsky11@gmail.com>
2014-07-10 16:51:55 -0400

    Update main.js

---
+++ main.js
@@ -0,0 +1,57 @@
+var side_open = false;
+var is_mobile = false;
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+ 	is_mobile = true;
+}
+
+if(is_mobile){
+  $("#side").css("width","80%");
+}
+
+function toggle_side(){
+	if(side_open){
+		side_open = false;
+		close_side();
+	}	
+	else{
+		side_open = true;
+		open_side();
+	}
+}
+
+function close_side(){
+	side_open = false;
+	$("#side").animate({
+		marginLeft: "-100%"
+	}, {
+		duration: 1000,
+		queue: false,
+		complete: function(){
+			$("#side").addClass("hide");
+		}
+	});
+	$("#detect").fadeOut();
+
+}
+
+function open_side(){
+	side_open = true;
+	$("#side").css("margin-left","-100%");
+	$("#side").removeClass("hide");
+	
+	$("#side").animate({
+  		marginLeft: "0%"
+  	}, {
+  		duration: 1000,
+  		queue: false
+  	});
+  	$("#detect").fadeIn();
+  }
+
+  $("#detect").click(function(){
+  	if(side_open){
+  		side_open = false;
+  		close_side();
+  	}	
+});
