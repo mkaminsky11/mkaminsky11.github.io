a64fdcc12402ef3ed2aaa9ae72e473a518e879bd
Michael Kaminsky <mkaminsky11@gmail.com>
2014-10-15 17:10:40 -0400

    update
    removes the toolbar (it’s not necessary) changes a lot of stylistic
    stuff.

--- js/main.js
+++ js/main.js
@@ -13,61 +13,9 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 }
 
 if(is_mobile){
-  $("#side").css("width","80%");
-  $(".goto").remove();
-}
-
-function toggle_side(){
-	if(!$("#side").hasClass("hide")){
-		side_open = false;
-		close_side();
-	}	
-	else{
-		side_open = true;
-		open_side();
-	}
-}
-
-function close_side(){
-	side_open = false;
-	$("#side").animate({
-		marginLeft: "-100%"
-	}, {
-		duration: 1000,
-		queue: false,
-		complete: function(){
-			$("#side").addClass("hide");
-		}
-	});
-	$("#detect").fadeOut();
 
 }
 
-function open_side(){
-	side_open = true;
-	$("#side").css("margin-left","-100%");
-	$("#side").removeClass("hide");
-	
-	$("#side").animate({
-  		marginLeft: "0%"
-  	}, {
-  		duration: 1000,
-  		queue: false
-  	});
-  	$("#detect").fadeIn();
-}
-$("#detect").click(function(){
-  	if(side_open){
-  		side_open = false;
-  		close_side();
-  	}	
-});
-$("#detect").click(function(){
-	if(!$("#side").hasClass("hide")){
-		side_open = false;
-		close_side();
-	}	
-});
 
 var editor = CodeMirror(document.getElementById("code"),{
     lineNumbers: true,
@@ -89,44 +37,3 @@ txtFile.onreadystatechange = function()
 	}
 }
 txtFile.send(null); 
-
-
-function goto(elem){
-   $(elem).velocity("scroll", { container: $("body") });
-}
-
-$("body").scroll(function(){
-	var top = $("#content").scrollTop();
-	if(top >= 80){
-		//name hidden	
-		$(".lang-goto").animate({
-		   marginTop: "-40px"
-		},{
-		  duration: 500,
-		  queue: false
-		});
-		
-		$(".name").animate({
-		   top: "0px"
-		},{
-		  duration: 500,
-		  queue: false
-		});
-	}
-	else{
-		//name visible
-		$(".lang-goto").animate({
-		   marginTop: "0px"
-		},{
-		  duration: 500,
-		  queue: false
-		});
-		
-		$(".name").animate({
-		   top: "40px"
-		},{
-		  duration: 500,
-		  queue: false
-		});
-	}
-});
