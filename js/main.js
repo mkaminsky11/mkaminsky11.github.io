6c264e40e1b2db8268bc8b8bed2ca2df5687b9ec
Michael Kaminsky <mkaminsky11@gmail.com>
2015-02-02 12:33:15 -0500

    show/hide

--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
 $(document).ready(function(){
-  var $timeline_block = $('.cd-timeline-block');
-
-  //hide timeline blocks which are outside the viewport
-  $timeline_block.each(function(){
-    if($(this).offset().top > $(window).scrollTop()+$(window).height()*0.75) {
-      $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
-    }
-  });
 
   //on scolling, show/animate timeline blocks when enter the viewport
   $(window).on('scroll', function(){
+    $(".cd-timeline-block").each(function(index){
+      if($(this).isOnScreen(0.4, 0.4)){
+        console.log("show");
+        $(this).velocity({
+          opacity: 1
+        },{
+          duration: 100
+        });
+      }
+      else{
+        console.log("hide");
+        $(this).velocity({
+          opacity: 0
+        },{
+          duration: 100
+        });
+      }
+    });
+
+    var $timeline_block = $('.cd-timeline-block');
+
+    //hide timeline blocks which are outside the viewport
+    $timeline_block.each(function(){
+      if($(this).offset().top > $(window).scrollTop()+$(window).height()*0.75) {
+        $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+      }
+    });
+
     $timeline_block.each(function(){
       if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
         $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
