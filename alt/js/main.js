62123dbb7b8a80d3134b66988d9210210678728d
Michael Kaminsky <mkaminsky11@gmail.com>
2015-02-26 07:46:07 -0500

    Update main.js

--- alt/js/main.js
+++ alt/js/main.js
@@ -9,7 +9,7 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var center = {x: 0, y:0};
 
-var max = Math.min($("#main").width() / 2, $("#main").height() / 2);
+var max = Math.min($("#main").width() / 2, $("#main").height() / 3);
 
 $(document).ready(function(){
     resize_main();
