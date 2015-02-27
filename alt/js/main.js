33c77e6865b8f31495b95952f9766e2f4a6aa947
Michael Kaminsky <mkaminsky11@gmail.com>
2015-02-27 14:40:23 -0500

    Update main.js

--- alt/js/main.js
+++ alt/js/main.js
@@ -176,7 +176,7 @@ function renderCircle(id, deg){
   con.lineWidth = 4;
   con.clearRect(0, 0, can.width, can.height);
   con.beginPath();
-  con.arc(can.width/2, can.height/2, can.width/2, -(Math.PI / 2), deg*(2 * Math.PI / 360) - Math.PI / 2, false);
+  con.arc(can.width/2, can.height/2, 25, -(Math.PI / 2), deg*(2 * Math.PI / 360) - Math.PI / 2, false);
   con.stroke();
 }
 function add(n){
