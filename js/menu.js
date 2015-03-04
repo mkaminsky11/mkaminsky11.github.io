aac8e0121e746ee2d6988bdd558e04baae30e768
Michael Kaminsky <mkaminsky11@gmail.com>
2015-03-04 14:15:50 -0500

    fix menu issues

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,31 @@
 var menu = {};
+menu.done = true;
 
 menu.toggle = function(){
-  $("#menu-dropdown").slideToggle();
+  if($("#menu-dropdown").css("display") === "none" && menu.done === true){
+    menu.done = false;
+    //show it
+    $("#menu-dropdown li").css("opacity",0);
+    $("#menu-dropdown").css("display","block");
+
+    $("#menu-dropdown li").velocity("transition.slideLeftIn", {
+      stagger: 70 ,
+      duration: 700,
+      complete: function(){
+        menu.done = true;
+      }
+    });
+  }
+  else if(menu.done === true){
+    menu.done = false;
+    //hide it
+    $("#menu-dropdown li").velocity("transition.slideLeftOut", {
+      stagger: 70 ,
+      duration: 700,
+      complete: function(){
+        $("#menu-dropdown").css("display","none");
+        menu.done = true;
+      }
+    });
+  }
 };
