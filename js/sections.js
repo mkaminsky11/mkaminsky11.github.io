7aaa00dcfff352164b56a9dd403c944bde5faf6a
mkaminsky11 <mkaminsky11@gmail.com>
2016-04-04 19:27:07 -0400

    add new content, make a little bit more responsive

--- js/sections.js
+++ js/sections.js
@@ -73,24 +73,44 @@ sections.backend = function(){
 };
 
 sections.resize = function(){
+  if($(window).width() > 600){
 
-  $("img.front-end").velocity({
-    translateX: "20%" ,
-    translateY: "-0%",
-    marginBottom: -1*$("img.front-end").height()
-  },{
-    duration: 100
-  });
+    $("img.front-end").velocity({
+      translateX: "20%" ,
+      translateY: "-0%",
+      marginBottom: -1*$("img.front-end").height()
+    },{
+      duration: 100
+    });
 
-  $("img.software").velocity({
-    translateX: "80%" ,
-    translateY: "-0%",
-    marginBottom: -1*$("img.software").height()
-  },{
-    duration: 100
-  });
+    $("img.front-end").attr("width","80%");
+    $("img.software").attr("width","40%");
+    $("img.back-end").attr("width","80%");
 
-  var margin = 0.15 * $("img.front-end").height() + 0.25 * $("img.software").height();
+    $("img.software").velocity({
+      translateX: "80%" ,
+      translateY: "-0%",
+      marginBottom: -1*$("img.software").height()
+    },{
+      duration: 100
+    });
 
-  $("#tilted-div").css("margin-top", margin);
+    var margin = 0.15 * $("img.front-end").height() + 0.25 * $("img.software").height();
+
+    $("#tilted-div").css("margin-top", margin);
+  }
+  else{
+    $("img.front-end").attr("width","100%");
+    $("img.front-end").css("margin-bottom","0");
+    $("img.front-end").velocity({
+      translateX: "0%"
+    });
+    $("img.software").attr("width","100%");
+    $("img.software").css("margin-bottom","0");
+    $("img.software").velocity({
+      translateX: "0%"
+    });
+    $("img.back-end").attr("width","100%");
+    $("img.back-end").css("margin-bottom","0");
+  }
 };
