07e25b39e62bcc1252ff5251f8b0acfaf0bdfddd
kmichael <kmichael@mit.edu>
2018-09-21 00:44:47 -0400

    complete rework
    Updated resume, new fonts, removed tabs + annoying animations, cleanup scripts, update text, proofreading (maybe), update projects and experience, remove timeline

--- js/main.js
+++ js/main.js
@@ -1,27 +1,17 @@
 $(window).resize(function(){
   resize_main();
   nodes.render();
-  tabs.resize();
 });
 var template;
 $(document).ready(function(){
+    light.init();
     resize_main();
-    circles.init();
-	tabs.init();
-	template = Handlebars.compile($('#project-template').html());
     window.setTimeout(function(){
         resize_main();
     }, 1000);
 });
 
 $(window).scroll(function(){
-      if($(window).scrollTop() > ($("#about").height() + 20)){
-        $("#about").css("opacity",0);
-      }
-      else{
-        $("#about").css("opacity",1);
-      }
-      inc.check();
 });
 
 
@@ -29,8 +19,6 @@ window.setInterval(nodes.render, 70);
 $('#canvas').css('background-color', 'rgba(94, 65, 47, 0)');
 
 function resize_main(){
-  $("#home").height($("body").height() - 50);
-  $("#home").width($("body").width());
   $("#canvas").attr("width",$("body").width()).attr("height",$("body").height());
   nodes.max = Math.min($("body").width() / 4, $("body").height() / 3);
   nodes.init();
